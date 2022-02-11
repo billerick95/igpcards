63eea293bcc6ecc1dfed8d2cd45be8687a72593c
Erick Felix <billerick95@gmail.com>
2022-02-12 02:52:06 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php include 'header.php';?>
+<?php include 'menu.php';?>
+
+
+<br><br><br><br>
+<div class="form-group">
+Empowering humanity.<br><hr>
+One human at a time.<br><br>
+We’re Coinmama, a financial service that makes it fast, safe and fun to buy digital currency, anywhere in the world. We believe that the future of money is one where we, the people, are in control of our own economy. A future where there’s no place for middle-men, hidden fees and fine print.<br><br>
+
+To deliver on that promise, we have come to work every day since 2013 to create the simplest financial service out there - spoken in a language you can understand, and backed by customer service you can count on.
+</div>
